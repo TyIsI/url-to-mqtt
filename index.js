23d82a4639d3da97305b3cd948ac335ff737b85b
Ty Eggen <tycho.eggen@gmail.com>
2024-05-21 12:25:41 -0700

    feat: refactor + switch to fetch
    plus improved env var sanity checking

--- index.js
+++ index.js
@@ -1,42 +1,57 @@
-const RP = require('request-promise')
 const MQTT = require('async-mqtt')
 
-// Functions
-const handleError = async (msg) => {
-  console.log(msg)
-  process.exit()
-}
+// Variables
+let lastLoadedContent = ''
+let lastUpdateSent = 0
 
-const connected = async () => {
-  console.log('Online')
-}
-
-const monitorURL = async () => {
-  try {
-    let forceUpdate = ((Date.now() - lastUpdateSent) > MQTT_INTERVAL)
-    let content = await RP(MONITOR_URL)
-    if (content !== lastLoadedContent || forceUpdate) {
-      await client.publish(MQTT_TOPIC, content)
-      lastLoadedContent = content
-      lastUpdateSent = Date.now()
-    }
-  } catch (e) {
-    handleError(e.stack)
-  }
-}
+if (process.env.MONITOR_URL == null) handleError('Missing MONITOR_URL')
+if (process.env.MONITOR_INTERVAL == null)
+    handleError('Missing MONITOR_INTERVAL')
+if (typeof process.env.MONITOR_INTERVAL !== 'string')
+    handleError('Invalid MONITOR_INTERVAL')
+if (process.env.MQTT_URI == null) handleError('Missing MQTT_URI')
+if (process.env.MQTT_TOPIC == null) handleError('Missing MQTT_TOPIC')
 
 // Main
-const MONITOR_URL = process.env.MONITOR_URL || handleError('Missing MONITOR_URL')
-const MONITOR_INTERVAL = parseInt(process.env.MONITOR_INTERVAL) || handleError('Missing MONITOR_INTERVAL')
-const MQTT_URI = process.env.MQTT_URI || handleError('Missing MQTT_URI')
-const MQTT_TOPIC = process.env.MQTT_TOPIC || handleError('Missing MQTT_TOPIC')
-const MQTT_INTERVAL = process.env.MQTT_INTERVAL ? parseInt(process.env.MQTT_INTERVAL) : MONITOR_INTERVAL
-
-var lastLoadedContent = ''
-var lastUpdateSent = 0
+const MONITOR_URL = process.env.MONITOR_URL
+const MONITOR_INTERVAL = parseInt(process.env.MONITOR_INTERVAL)
+const MQTT_URI = process.env.MQTT_URI
+const MQTT_TOPIC = process.env.MQTT_TOPIC
+const MQTT_INTERVAL = process.env.MQTT_INTERVAL
+    ? parseInt(process.env.MQTT_INTERVAL)
+    : MONITOR_INTERVAL
 
 const client = MQTT.connect(MQTT_URI)
 
 client.on('connect', connected)
 
 setInterval(monitorURL, MONITOR_INTERVAL)
+
+// Functions
+async function connected() {
+    console.log('Online')
+}
+
+async function monitorURL() {
+    try {
+        const forceUpdate = Date.now() - lastUpdateSent > MQTT_INTERVAL
+
+        const content = await (await fetch(MONITOR_URL)).text()
+
+        if (content !== lastLoadedContent || forceUpdate) {
+            await client.publish(MQTT_TOPIC, content)
+
+            lastLoadedContent = content
+
+            lastUpdateSent = Date.now()
+        }
+    } catch (e) {
+        handleError(e.stack)
+    }
+}
+
+function handleError(...args) {
+    console.error(...args)
+
+    process.exit(255)
+}
